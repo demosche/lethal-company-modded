f9608d1c21e189fabb91332085eda5e2d6ad12e0
Maksim U <20mingot@gmail.com>
2024-01-05 16:03:43 +0600

    Make generator use markdown newlines and display message when the job is done

--- modListGenerator.ps1
+++ modListGenerator.ps1
@@ -1,13 +1,14 @@
 $reader = New-Object System.IO.StreamReader(Get-ChildItem $PSScriptRoot"\ModdedBuild\mods.yml")
 
 $lines = ''
-$lines+="Drop this profile here`n"
+$lines+="Drop this profile here<br>"
 $lines+="C:\Users\{USERNAME}\AppData\Roaming\r2modmanPlus-local\LethalCompany\profiles\`n`n"
-$lines+="List of mods:`n"
+$lines+="List of mods:<br>"
+$lines+="`n>"
+$c=0
 
 if ($reader -ne $null) {
     while (!$reader.EndOfStream) {
-	$c=0
         $line = $reader.ReadLine()
 	$modname=''
 	$version=' '
@@ -25,11 +26,13 @@ if ($reader -ne $null) {
             $lines+=$modname+$version.trim()
         }
 	if ($line.Contains("    patch: ")) {
-	        $version = $version+'.'	
+	    $version = $version+'.'	
             $version = $version+ $line -replace '    patch: ', ''
-	        $lines+=$modname+$version.trim()+"`n"
+	    $lines+=$modname+$version.trim()+"<br>`n"
+	    $c+=1
         }
 
     }
 }
 $lines | Out-File -FilePath $PSScriptRoot"\README.md"
+Write-Host "The mod list is updated." "Number of mods is"$c
